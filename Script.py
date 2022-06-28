5374ab1a54479c2a17e983f162ab1aa0f010a97a
PrinceStarLord <86295965+PrinceStarLord@users.noreply.github.com>
2022-06-28 03:33:38 -0500

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,6 +1,6 @@
 class script(object):
     START_TXT = """<b>Hᴇʟʟᴏ {},
-ᴍʏ ɴᴀᴍᴇ ɪs </b><a href=https://t.me/{}><b>{}</b></a><b>,𝙸 ᴄᴀɴ ᴘʀᴏᴠɪᴅᴇ ᴍᴏᴠɪᴇs 🎥 ᴊᴜsᴛ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ</b>"""
+ᴍʏ ɴᴀᴍᴇ ɪs </b><a href=https://t.me/{}><b>{}</b></a><b>,𝙸 ᴄᴀɴ ᴘʀᴏᴠɪᴅᴇ ᴍᴏᴠɪᴇs ᴊᴜsᴛ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ</b>"""
     HELP_TXT = """<b>𝙷𝙴𝚈 {},
 ʜᴇʀᴇ ɪs ᴛʜᴇ ʜᴇʟᴘ ᴄᴏᴍᴍᴀɴᴅs.<b/>"""
     ABOUT_TXT = """❤️ <b>Mʏ Nᴀᴍᴇ : {}
@@ -22,7 +22,7 @@ Fɪʟᴛᴇʀ ɪs ᴛʜᴇ Fᴇᴀᴛᴜʀᴇ ᴡᴇʀᴇ ᴜsᴇʀs ᴄᴀɴ s�
 • /filters - <code>list all the filters of a chat</code>
 • /del - <code>delete a specific filter in chat</code>
 • /delall - <code>delete the whole filters in a chat (chat owner only)</code>"""
-    BUTTON_TXT = """𝙷ᴇʟᴘ: <b>Buttons</b>
+    BUTTON_TXT = """<b>𝙷ᴇʟᴘ: Buttons</b>
 
 ɪ sᴜᴘᴘᴏʀᴛs ʙᴏᴛʜ ᴜʀʟ ᴀɴᴅ ᴀʟᴇʀᴛ ɪɴʟɪɴᴇ ʙᴜᴛᴛᴏɴs.
 
@@ -51,7 +51,7 @@ Fɪʟᴛᴇʀ ɪs ᴛʜᴇ Fᴇᴀᴛᴜʀᴇ ᴡᴇʀᴇ ᴜsᴇʀs ᴄᴀɴ s�
 𝟷. ᴏɴʟʏ ᴀᴅᴍɪɴs ᴄᴀɴ ᴀᴅᴅ ᴀ ᴄᴏɴɴᴇᴄᴛɪᴏɴ.
 𝟸. sᴇɴᴅ <code>/connect</code> Fᴏʀ ᴄᴏɴɴᴇᴄᴛɪɴɢ ᴍᴇ ᴛᴏ ᴜʀ ᴘᴍ
 
-<b>:ᴄᴏᴍᴍᴀɴᴅs ᴀɴᴅ ᴜsᴀɢᴇ</b>
+<b>ᴄᴏᴍᴍᴀɴᴅs ᴀɴᴅ ᴜsᴀɢᴇ</b>
 • /connect  - <code>connect a particular chat to your PM</code>
 • /disconnect  - <code>disconnect from a chat</code>
 • /connections - <code>list all your connections</code>"""
@@ -79,11 +79,11 @@ This module only works for my admins
 • /unban  - <code>to unban a user.</code>
 • /channel - <code>to get list of total connected channels</code>
 • /broadcast - <code>to broadcast a message to all users</code>"""
-    STATUS_TXT = """🎥 T𝙾𝚃𝙰𝙻 𝙵𝙸𝙻𝙴𝚂  : <code>{}</code>
-🙎‍♂️ 𝚃𝙾𝚃𝙰𝙻 𝚄𝚂𝙴𝚁𝚂 : <code>{}</code>
-📊 𝚃𝙾𝚃𝙰𝙻 𝙲𝙷𝙰𝚃𝚂 : <code>{}</code>
-⏹ 𝚄𝚂𝙴𝙳 𝚂𝚃𝙾𝚁𝙰𝙶𝙴: <code>{}</code> 
-✅ 𝙵𝚁𝙴𝙴 𝚂𝚃𝙾𝚁𝙰𝙶𝙴: <code>{}</code> """
+    STATUS_TXT = """<b>• T𝙾𝚃𝙰𝙻 𝙵𝙸𝙻𝙴𝚂  : </b><code>{}</code>
+<b>• 𝚃𝙾𝚃𝙰𝙻 𝚄𝚂𝙴𝚁𝚂 : </b><code>{}</code>
+<b>• 𝚃𝙾𝚃𝙰𝙻 𝙲𝙷𝙰𝚃𝚂 : </b><code>{}</code>
+<b>• 𝚄𝚂𝙴𝙳 𝚂𝚃𝙾𝚁𝙰𝙶𝙴: </b><code>{}</code> 
+<b>• 𝙵𝚁𝙴𝙴 𝚂𝚃𝙾𝚁𝙰𝙶𝙴: </b><code>{}</code> """
     LOG_TEXT_G = """#NewGroup
 Group = {}(<code>{}</code>)
 Total Members = <code>{}</code>
