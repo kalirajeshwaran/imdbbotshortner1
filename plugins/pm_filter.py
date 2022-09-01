de5e569c344ea9c59499958bd11efd142d2903d5
BELUGA_XD <96910602+belugaop@users.noreply.github.com>
2022-09-01 12:20:43 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -2,7 +2,8 @@
 import asyncio
 import re
 import ast
-
+import requests
+import json
 from pyrogram.errors.exceptions.bad_request_400 import MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty
 from Script import script
 import pyrogram
